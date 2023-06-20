73db2ce1955ceb65cb25a4cf04d002eef67827e2
srpkdyy <srpkdyy@gmail.com>
2023-06-20 22:09:31 +0900

    add quantizer

--- autoencoders/vq_vae/vq_vae.py
+++ autoencoders/vq_vae/vq_vae.py
@@ -1,8 +1,10 @@
 import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
 
 
 class VQVAE(nn.Module):
-    def __init__(self, channels=3, k_cat=512, dim=256):
+    def __init__(self, channels=3, k_cat=512, dim=256, beta=0.25):
         super().__init__()
 
         self.encoder = nn.Sequential(
@@ -12,7 +14,7 @@ class VQVAE(nn.Module):
             ResBlock(dim)
         )
 
-        self.quantizer = Quantizer(k_cat, dim)
+        self.quantizer = Quantizer(k_cat, dim, beta)
 
         self.decoder = nn.Sequential(
             ResBlock(dim),
@@ -23,10 +25,10 @@ class VQVAE(nn.Module):
 
     def forward(self, x):
         x = self.encoder(x)
-        x = self.quantizer(x)
+        x, loss = self.quantizer(x)
         x = self.decoder(x)
 
-        return x
+        return {'recon': x, 'vq_loss': loss}
 
 
 class Downsampler(nn.Module):
@@ -65,11 +67,34 @@ class ResBlock(nn.Module):
 
 
 class Quantizer(nn.Module):
-    def __init__(self, k_cat, dim):
+    def __init__(self, k_cat, dim, beta):
         super().__init__()
 
         self.emb = nn.Embedding(k_cat, dim)
+        self.beta = beta
 
     def forward(self, x):
-        return x
+        b, c, h, w = x.shape
+
+        ze = rearrange(x, 'b c h w -> (b h w) 1 c')
+        ej = rearrange(self.emb.weight, 'k c -> 1 k c')
+
+        l2 = (ze - ej).pow(2).sum(-1).sqrt()
+        nearest_ids = l2.argmin(dim=-1)
+
+        zq = self.emb(nearest_ids)
+        zq = rearrange(zq, '(b h w) c -> b c h w', b=b, h=h)
+
+        vq_loss = F.mse_loss(x.detach(), zq) + self.beta * F.mse_loss(x, zq.detach())
+
+        zq = (zq - x).detach() + x
+
+        return zq, vq_loss
+
+
+if __name__ == '__main__':
+    import torch
+    vqvae = VQVAE()
+    x = torch.rand(1, 3, 224, 224)
+    print(vqvae(x))
 
